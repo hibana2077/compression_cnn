ad0eaccd083628b0989b0bbf371c335c0eece012
hibana2077 <hibana2077@gmail.com>
2024-07-02 11:46:29 +0800

    Update model architecture to use "resnet101" model from timm library

--- src/cifar10_exp/main.py
+++ src/cifar10_exp/main.py
@@ -64,8 +64,9 @@ print('==> Building model..')
 # net,net_name = QuadCompression(), 'QuadCompression'
 # net,net_name = timm.create_model('convnextv2_tiny',num_classes=10), 'convnextv2_tiny'
 # net,net_name = timm.create_model('seresnet18',num_classes=10), "seresnet18"
+net,net_name = timm.create_model('resnet101',num_classes=10), "resnet101"
 # net,net_name = timm.create_model('efficientnet_b0',num_classes=10), "efficientnet_b0"
-net,net_name = timm.create_model('regnetx_080',num_classes=10), "regnetx_080"
+# net,net_name = timm.create_model('regnetx_080',num_classes=10), "regnetx_080"
 # net,net_name = timm.create_model('convnext_nano',num_classes=10), "convnext_nano"
 # net,net_name = timm.create_model('convnext_pico',num_classes=10), "convnext_pico"
 print('Number of parameters(M):', sum(p.numel() for p in net.parameters()) / 1e6)
